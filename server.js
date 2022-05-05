f6d92fd417efcad380f396fe4f45081d3b1eb0cc
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-05-05 10:12:27 +0200

    Setup Express server

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const createError = require('http-errors');
+const path = require('path');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Hello Back end!');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, () => `Server is running on port ${port}`);
+
+module.exports = app;
