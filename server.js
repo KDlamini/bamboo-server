f8c07c4e6d3263fe07db3f40cec484b36b82f005
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-05-05 12:22:15 +0200

    Connect server to MongoDb Atlas

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 const createError = require('http-errors');
 const path = require('path');
+const mongoose = require('mongoose');
+require('dotenv').config()
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+mongoose.connect(process.env.BD_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
