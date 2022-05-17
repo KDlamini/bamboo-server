1da4beb932a62ad5fcb4e48f25484e70f1e66db2
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-05-17 14:43:02 +0200

    Add products model

---
+++ models/product.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    name: { type: String, required: true },
+    comment: { type: String },
+    rating: { type: Number, required: true }
+}, { timeStamps: true });
+
+const productSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    rating: { type: Number, required: true },
+    description: { type: String, required: true },
+    image: { type: String, required: true },
+    category: { type: String, required: true },
+    countInStock: { type: Number, required: true },
+    reviews: [reviewSchema],
+}, { timeStamps: true });
+
+module.exports = mongoose.model('products', productSchema);
