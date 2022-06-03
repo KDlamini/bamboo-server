22c28a28954479811b239c2dfa66db7728c974b6
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-06-03 13:26:09 +0200

    Update product model

--- models/product.js
+++ models/product.js
@@ -10,6 +10,7 @@ const reviewSchema = new mongoose.Schema({
 const productSchema = mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
+    type: { type: String, required: true },
     rating: { type: Number, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
