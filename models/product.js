6796f05037a96bb99fc4b0cf41239e4657b6a4c0
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-06-27 20:01:53 +0200

    Add more Product attributes on SChema

--- models/product.js
+++ models/product.js
@@ -7,14 +7,23 @@ const reviewSchema = new mongoose.Schema({
     rating: { type: Number, required: true }
 }, { timeStamps: true });
 
+const dealsSchema = new mongoose.Schema({
+    available: { type: Boolean, required: true },
+    discount: { type: Number, required: true }
+});
+
 const productSchema = mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
     type: { type: String, required: true },
+    brand: { type: String, required: true },
+    color: { type: String, required: true },
     rating: { type: Number, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true },
+    department: { type: String, required: true },
     category: { type: String, required: true },
+    deals: [dealsSchema],
     countInStock: { type: Number, required: true },
     reviews: [reviewSchema],
 }, { timeStamps: true });
