2c366f079479489c51c5a24189cc48b46e40400b
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-06 17:28:54 +0200

    Add review createdAt date

--- models/product.js
+++ models/product.js
@@ -4,7 +4,8 @@ const reviewSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     name: { type: String, required: true },
     comment: { type: String },
-    rating: { type: Number, required: true }
+    rating: { type: Number, required: true },
+    createdAt: { type: Date, default: Date.now }
 }, { timeStamps: true });
 
 const dealsSchema = new mongoose.Schema({
