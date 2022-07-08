4ef8c54e5a008017ab68ab64997e65a57cab3a5c
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-08 21:47:16 +0200

    Add create new user route

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const { getUser, getUsers } = require('../controllers/fetchUsers');
+const createUser = require('../controllers/createUser');
 
 router.get('/', getUsers);
 router.get('/:id', getUser);
+router.post('/', createUser);
 
 module.exports = router;
