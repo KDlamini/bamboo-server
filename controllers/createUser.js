44d015da71118937050e51d5a4bea30fa36fcddf
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-08 23:11:25 +0200

    Update createUser error handling

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const Users = require("../models/user");
 
@@ -23,41 +24,36 @@ const createUser = async (req, res) => {
     password,
   });
 
-  try {
-    //Create salt & hash
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(user.password, salt, async (err, hash) => {
-        if (err) throw err;
+  //Create salt & hash
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, async (err, hash) => {
+      if (err) throw err;
 
-        user.password = hash;
-        const newUser = await user.save();
+      user.password = hash;
+      const newUser = await user.save();
 
-        //Create JWT
-        jwt.sign(
-          { id: newUser._id },
-          process.env.JWT_SECRET,
-          { expiresIn: 3600 },
-          (err, token) => {
-            if (err) throw err;
+      //Create JWT
+      jwt.sign(
+        { id: newUser._id },
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
 
-            res.status(200).json({
-              token,
-              user: {
-                type: "user",
-                id: newUser._id,
-                name: newUser.name,
-                email: newUser.email,
-              },
-            });
-          }
-        );
+          res.status(200).json({
+            token,
+            user: {
+              type: "user",
+              id: newUser._id,
+              name: newUser.name,
+              email: newUser.email,
+            },
+          });
+        }
+      );
 
-      });
     });
-
-  } catch (error) {
-    res.status(400).json({ message: error.message});
-  }
+  });
 };
 
 module.exports = createUser;
