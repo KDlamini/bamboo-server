5da03cb76d20339f23db37af5fa19d7bfb4e6ddb
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-12 15:17:06 +0200

    Add address schema

--- models/user.js
+++ models/user.js
@@ -1,34 +1,25 @@
 const mongoose = require("mongoose");
 
+const addressSchema = new mongoose.Schema({
+  username: { type: String},
+  phone: { type: String},
+  house_name: { type: String },
+  street: { type: String },
+  city: { type: String },
+  state: { type: String },
+  zip: { type: String },
+  createdAt: { type: Date, default: Date.now }
+});
+
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true, 
-  },
-  surname: {
-    type: String,
-    required: true, 
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  role: {
-    type: String,
-    default: "user",
-  },
-  register_date: {
-    type: Date,
-    default: Date.now,
-  },
+  name: { type: String, required: true },
+  surname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  billing_address: [addressSchema],
+  image: { type: String },
+  role: { type: String, default: "user" },
+  register_date: { type: Date, default: Date.now},
 });
 
 const User = mongoose.model("User", userSchema);
