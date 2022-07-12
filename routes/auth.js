a7c8928248af053db7497f6314597fd666199a6c
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-12 15:17:26 +0200

    authenticate post address route

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const auth = require('../middleware/auth');
 const router = express.Router();
 
 const { authUser, getAuthUser } = require('../controllers/authUser');
+const postAddress = require('../controllers/postAddress');
 
 // @route   POST auth/login
 // @access  Public
@@ -12,5 +13,9 @@ router.post('/login', authUser);
 // @route   GET auth/user
 // @access  Private
 router.get('/user', auth, getAuthUser);
+
+// @route   POST auth/user/:id/add_address
+// @access  Private
+router.post('/user/:id/add_address', auth, postAddress);
 
 module.exports = router;
