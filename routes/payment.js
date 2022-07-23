0310e8d1ed00ca1b9cf3597f4af941836c3daf3a
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-23 20:21:42 +0200

    Update routes

--- routes/payment.js
+++ routes/payment.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const router = express.Router();
 
-const createPayment = require('../controllers/payments');
+const { createPayment, eventHook } = require('../controllers/payments');
 
 // @route   POST payment/create
 // @access  Public
 router.post('/stripe/create-checkout-session', createPayment);
+
+// @route   POST payment/webhook
+// @access  Public
+router.post('/webhook', bodyParser.raw({type: 'application/json'}), eventHook);
 
 module.exports = router;
