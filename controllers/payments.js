eec4f9d0220e180d142426fe685fee37a49382ba
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-23 20:21:24 +0200

    Update payment controllers

--- controllers/payments.js
+++ controllers/payments.js
@@ -6,7 +6,40 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const createPayment = async (req, res) => {
 
     try {
-        const { order } = req.body;
+        const { userId, address, order } = req.body;
+        const {
+            username, phone, house_name: houseName, street, city, state, zip,
+        } = address;
+
+        const customer = await stripe.customers.create({
+            name: username,
+            phone,
+            address: {
+                city,
+                line1: houseName,
+                line2: street,
+                postal_code: zip,
+                state: state
+            },
+            currency: 'usd',
+            metadata: { 
+                id: userId,
+                cart: JSON.stringify(order.map(item => {
+                   return {
+                    _id: item._id,
+                    type: item.type,
+                    name: item.name,
+                    brand: item.brand,
+                    color: item.color,
+                    description: item.description,
+                    price: item.price,
+                    discountPrice: item.discountPrice,
+                    quantity: item.quantity,
+                    image: item.image,
+                }
+                })),
+            }
+        });
 
         const line_items = order.map(item => {
             return {
@@ -32,6 +65,7 @@ const createPayment = async (req, res) => {
 
         const session = await stripe.checkout.sessions.create({
             line_items,
+            customer: customer.id,
             mode: 'payment',
             success_url: `${process.env.CLIENT_URL}/order_receipt`,
             cancel_url: `${process.env.CLIENT_URL}/review_order`,
@@ -43,4 +77,41 @@ const createPayment = async (req, res) => {
     }
 }
 
-module.exports = createPayment
+const eventHook = (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event, data, eventType;
+
+//   try {
+//     event = stripe.webhooks.constructEvent(request.body, sig, process.env.ENDPOINT_SECRET);
+//     console.log("Webhook verified: ",event);
+//   } catch (err) {
+//     console.log(`Webhook Error: ${err.message}`);
+//     response.status(400).send(`Webhook Error: ${err.message}`);
+//     return;
+//   }
+
+//   data = event.data.object;
+//   eventType = event.type;
+
+    data = request.body.data.object;
+    eventType = request.body.type;
+
+  // Handle the event
+    if (eventType === 'checkout.session.completed') {
+        stripe.customers.retrieve(data.customer).then(customer => {
+            console.log(customer);
+            console.log("data: ", data);
+        }
+        ).catch(err => {
+            console.log(err);
+        }
+        );
+    }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send().end();
+};
+
+
+module.exports = { createPayment, eventHook };
