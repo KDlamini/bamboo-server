a00e1ca6c158d8adb03ac035c5a8e092a3c6b497
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-08-03 14:42:59 +0200

    Add fetch orders route

--- routes/payment.js
+++ routes/payment.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const auth = require('../middleware/auth');
 
 const router = express.Router();
 
 const { createPayment, eventHook } = require('../controllers/payments');
+const getOrders  = require('../controllers/getOrders');
 
 // @route   POST payment/create
 // @access  Public
@@ -12,5 +14,9 @@ router.post('/stripe/create-checkout-session', createPayment);
 // @route   POST payment/webhook
 // @access  Public
 router.post('/webhook', bodyParser.raw({type: 'application/json'}), eventHook);
+
+// @route   GET payment/orders
+// @access  Public
+router.get('/orders', auth, getOrders);
 
 module.exports = router;
